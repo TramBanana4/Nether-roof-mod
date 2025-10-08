729b469fbeb13eb664ae916a54a429e58c9c0113
justagut <jortsanderman@gmail.com>
2025-10-08 14:40:29 +0200

    made the structure very long for testing purposes

--- src/main/java/com/justagut/NetherRoofMod/loopontick/ontick.java
+++ src/main/java/com/justagut/NetherRoofMod/loopontick/ontick.java
@@ -24,38 +24,13 @@ public class ontick {
 
     @SubscribeEvent
     public static void onServerStarted(ServerStartingEvent event){
-        System.out.println("started");
-        System.out.println("started");
-        System.out.println("started");
 
 
     }
 
     @SubscribeEvent
     public static void serverTick(ServerTickEvent.Pre event){
-        MinecraftServer server = event.getServer();
-        ServerLevel overworld = server.getLevel(Level.OVERWORLD);
 
-
-
-
-        int count;
-        for (count = 0;count < overworld.players().size();count++){
-
-            int playerx = overworld.players().get(count).chunkPosition().x * 16;
-            int playerz = overworld.players().get(count).chunkPosition().z * 16;
-            if (!alrdonechunks.contains(playerx + "," + playerz)) {
-                alrdonechunks.add(playerx + "," + playerz);
-                for (int i = (int)(Math.random() * 6);i < 5;i++){
-                BlockPos oregenpos = new BlockPos(playerx + (int)(Math.random() * 17) - 8,(int)(Math.random() * 8) - 63,playerz + (int)(Math.random() * 17));
-                if (overworld.getBlockState(oregenpos).getBlock() == Blocks.DEEPSLATE)
-                {
-                    overworld.setBlockAndUpdate(oregenpos,ModBlocks.BISMUTH_DEEPSLATE_ORE.get().defaultBlockState());
-                    System.out.println(oregenpos);
-                }
-                }
-                }
-            }
         }
 
 
@@ -63,8 +38,6 @@ public class ontick {
 
     @SubscribeEvent
     public static void livingDamage(LivingDamageEvent.Pre event) {
-        if (event.getSource().getDirectEntity() instanceof Player player) {
-            player.sendSystemMessage(Component.literal("hoi"));
-        }
+
     }
 }
